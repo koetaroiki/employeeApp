64524051795a8d55cb17c803a199072657259472
koetaroiki <koetaroiki@outlook.com>
2026-08-01 02:04:26 +0900

    コメント整理

--- src/main/java/model/Employee.java
+++ src/main/java/model/Employee.java
@@ -1,11 +1,39 @@
 package model;
 
+/**
+ * 社員情報を保持するモデルクラス
+ * 
+ * データベースの employees テーブルの1件分のデータを
+ * Javaオブジェクトとして扱うためのクラス
+ */
 public class Employee {
+
+  // 社員ID
   private int id;
+
+  // 社員名
   private String name;
+
+  // 年齢
   private int age;
+
+  // 所属部署
   private String department;
 
+  /**
+   * JavaBeans形式で使用するための引数なしコンストラクタ
+   */
+  public Employee() {
+  }
+
+  /**
+   * 社員情報を初期化するコンストラクタ
+   *
+   * @param id         社員ID
+   * @param name       社員名
+   * @param age        年齢
+   * @param department 所属部署
+   */
   public Employee(int id, String name, int age, String department) {
     this.id = id;
     this.name = name;
@@ -13,22 +41,47 @@ public class Employee {
     this.department = department;
   }
 
+  /**
+   * 社員IDを取得する
+   *
+   * @return 社員ID
+   */
   public int getId() {
     return id;
   }
 
+  /**
+   * 社員名を取得する
+   *
+   * @return 社員名
+   */
   public String getName() {
     return name;
   }
 
+  /**
+   * 年齢を取得する
+   *
+   * @return 年齢
+   */
   public int getAge() {
     return age;
   }
 
+  /**
+   * 所属部署を取得する
+   *
+   * @return 所属部署
+   */
   public String getDepartment() {
     return department;
   }
 
+  /**
+   * Employeeオブジェクトの内容を文字列として表示する
+   *
+   * @return 社員情報の文字列
+   */
   @Override
   public String toString() {
     return "Employee [id=" + id
