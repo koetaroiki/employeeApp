bd3845136e594224c712d837a4dea3aac503fb43
koetaroiki <koetaroiki@outlook.com>
2026-08-02 20:28:43 +0900

    DAOにコメントを追加

--- src/main/java/dao/EmployeeDAO.java
+++ src/main/java/dao/EmployeeDAO.java
@@ -10,26 +10,45 @@ import java.util.List;
 import model.Employee;
 import util.DBUtil;
 
+/**
+ * employeesテーブルを操作するDAOクラス
+ */
 public class EmployeeDAO {
+
+  /**
+   * employeesテーブルの全件を取得する
+   *
+   * @return 社員情報のリスト
+   */
   public List<Employee> findAll() {
 
+    // Employeeオブジェクトを格納するリスト
     List<Employee> employees = new ArrayList<>();
 
+    // 実行するSQL
     String sql = "SELECT id, name, age, department FROM employees";
 
     try (
+        // データベースへ接続
         Connection connection = DBUtil.getConnection();
+
+        // SQLを準備
         PreparedStatement statement = connection.prepareStatement(sql);
+
+        // SQLを実行し、結果を取得
         ResultSet resultSet = statement.executeQuery()) {
 
+      // 検索結果を1件ずつ取り出す
       while (resultSet.next()) {
 
+        // 取得した1件分のデータをEmployeeオブジェクトに変換
         Employee employee = new Employee(
             resultSet.getInt("id"),
             resultSet.getString("name"),
             resultSet.getInt("age"),
             resultSet.getString("department"));
 
+        // リストへ追加
         employees.add(employee);
       }
 
@@ -37,6 +56,7 @@ public class EmployeeDAO {
       e.printStackTrace();
     }
 
+    // 取得した社員一覧を返す
     return employees;
   }
 }
