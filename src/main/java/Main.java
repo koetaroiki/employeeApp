0f60df0399baacad561dc406c746d08afca0ec8b
koetaroiki <koetaroiki@outlook.com>
2026-08-04 23:50:05 +0900

    Mainクラスにコメントを追加

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,25 @@
-
 import java.util.List;
 
 import dao.EmployeeDAO;
 import model.Employee;
 
+/**
+ * アプリケーションの実行クラス
+ */
 public class Main {
 
   public static void main(String[] args) {
 
+    // EmployeeDAOを生成
     EmployeeDAO dao = new EmployeeDAO();
 
+    // 社員情報をデータベースから取得
     List<Employee> employees = dao.findAll();
 
+    // 取得件数を表示
     System.out.println("取得件数：" + employees.size());
 
+    // 取得した社員情報を1件ずつ表示
     for (Employee employee : employees) {
       System.out.println(employee);
     }
